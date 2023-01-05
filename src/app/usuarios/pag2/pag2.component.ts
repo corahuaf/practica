36fb209c5849c357280bc45ddd8776d0e1d370aa
Fernando <corahuaf@gmail.com>
2023-01-05 12:57:31 -0500

    diseño contact

--- src/app/usuarios/pag2/pag2.component.ts
+++ src/app/usuarios/pag2/pag2.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./pag2.component.css']
 })
 export class Pag2Component {
-
+  experiencia = [
+  {
+    nombre: "juan",
+    apellido: "as"
+  },
+  {
+    nombre: "ric",
+    apellido: "asw"
+  }
+  ];
 }
